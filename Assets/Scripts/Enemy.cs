e1dfd46c132ee38f3ad44015f45fbfa040b1dc55
Enojia <bricechanelaw@gmail.com>
2015-10-01 16:12:58 +0200

    making mostly enemy script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public GameObject player;
+
+    private Rigidbody2D rb2D;
+	// Use this for initialization
+	void Start ()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 toPlayer = player.transform.position - transform.position;
+        float z = Mathf.Atan2(toPlayer.y, toPlayer.x) * Mathf.Rad2Deg - 90;
+
+        transform.eulerAngles = new Vector3(0, 0, z);
+        rb2D.AddForce(gameObject.transform.up * speed);
+	}
+}
---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class MovingObject : MonoBehaviour
+{
+    public float speed;
+    
+    protected Rigidbody2D rb2D;
+
+    // Use this for initialization
+    protected virtual void Start ()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+	
+
+    protected void Rotate(Vector3 end)
+    {
+        Quaternion rot = Quaternion.LookRotation(transform.position - end, Vector3.forward);
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+    }
+}
